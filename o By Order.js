6f291e455af35f0a233715783e91bcca665d9331
Lucien Lin <99335309+Lucien-MHL@users.noreply.github.com>
2023-01-19 11:14:57 +0800

    Create o By Order.js

---
+++ o By Order.js
@@ -0,0 +1,33 @@
+// Please Clone this before start!!
+/* a, b, c 是三個 request 的 callback function，
+** 分別會延遲 500, 200 300 毫秒回來
+** 執行 doByOrder 之後會後依序印出 b, c, a
+** 希望能改成印出 a, b, c
+** 
+*/ 
+
+
+// DONT' change a, b, c function
+const a = callback => {
+  setTimeout(() => { callback('a'); }, 500);
+};
+
+const b = callback => {
+  setTimeout(() => { callback('b'); }, 200);
+};
+
+const c = callback => {
+  setTimeout(() => { callback('c'); }, 300);
+};
+
+const tasks = [a, b, c];
+
+
+const doByOrder = (tasks, callback) => {
+  a(callback);
+  b(callback);
+  c(callback);
+  // modify this function to log "a" => "b" => "c"
+}
+
+doByOrder(tasks, console.log.bind(console));
